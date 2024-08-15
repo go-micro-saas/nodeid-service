5062ccc1ea0ab5ad14b6412e358b1273b57d0891
ikaiguang <ikaiguang@163.com>
2024-08-16 06:15:36 +0800

    feat: 配置

--- app/nodeid-service/cmd/makefile_run.mk
+++ app/nodeid-service/cmd/makefile_run.mk
@@ -1,5 +1,9 @@
-# ping-service
 .PHONY: run-database-migration
-# run service :-->: run database-migration
+# run :-->: run database-migration
 run-database-migration:
 	go run ./app/nodeid-service/cmd/database-migration/... -conf=./app/nodeid-service/configs
+
+.PHONY: store-configuration
+# run :-->: run database-migration
+store-configuration:
+	go run ./app/nodeid-service/cmd/store-configuration/... -conf=./app/nodeid-service/configs
---
+++ app/nodeid-service/cmd/store-configuration/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "flag"
+
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Version is the version of the compiled software.
+	Version string
+
+	// flagconf is the config flag.
+	flagconf string
+)
+
+func init() {
+	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
+}
+
+// go run ./cmd/migration/... -conf=./configs
+func main() {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+}
