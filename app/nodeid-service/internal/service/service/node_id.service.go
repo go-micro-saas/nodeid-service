0aa046e09399bfa52c96202ca9db0634ca4b98be
ikaiguang <ikaiguang@163.com>
2024-08-25 21:15:25 +0800

    feat: ping message

--- app/nodeid-service/internal/service/service/node_id.service.go
+++ app/nodeid-service/internal/service/service/node_id.service.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"context"
-	resources "github.com/go-micro-saas/nodeid-service/api/nodeid-service/v1/resources"
+	resourcev1 "github.com/go-micro-saas/nodeid-service/api/nodeid-service/v1/resources"
 	servicev1 "github.com/go-micro-saas/nodeid-service/api/nodeid-service/v1/services"
 )
 
@@ -11,26 +11,28 @@ type nodeIDV1Service struct {
 }
 
 // Ping ping
-func (s *nodeIDV1Service) Ping(context.Context, *resources.PingReq) (*resources.PingResp, error) {
-	return &resources.PingResp{}, nil
+func (s *nodeIDV1Service) Ping(ctx context.Context, req *resourcev1.PingReq) (*resourcev1.PingResp, error) {
+	return &resourcev1.PingResp{Data: &resourcev1.PingRespData{
+		Message: "request message: " + req.Message,
+	}}, nil
 }
 
 // GetServiceInfo 获取服务信息
-func (s *nodeIDV1Service) GetServiceInfo(context.Context, *resources.GetServiceInfoReq) (*resources.GetServiceInfoResp, error) {
-	return &resources.GetServiceInfoResp{}, nil
+func (s *nodeIDV1Service) GetServiceInfo(ctx context.Context, req *resourcev1.GetServiceInfoReq) (*resourcev1.GetServiceInfoResp, error) {
+	return &resourcev1.GetServiceInfoResp{}, nil
 }
 
 // GetNodeId 获取节点id
-func (s *nodeIDV1Service) GetNodeId(context.Context, *resources.GetNodeIdReq) (*resources.GetNodeIdResp, error) {
-	return &resources.GetNodeIdResp{}, nil
+func (s *nodeIDV1Service) GetNodeId(ctx context.Context, req *resourcev1.GetNodeIdReq) (*resourcev1.GetNodeIdResp, error) {
+	return &resourcev1.GetNodeIdResp{}, nil
 }
 
 // RenewalNodeId 续订节点id
-func (s *nodeIDV1Service) RenewalNodeId(context.Context, *resources.RenewalNodeIdReq) (*resources.RenewalNodeIdResp, error) {
-	return &resources.RenewalNodeIdResp{}, nil
+func (s *nodeIDV1Service) RenewalNodeId(ctx context.Context, req *resourcev1.RenewalNodeIdReq) (*resourcev1.RenewalNodeIdResp, error) {
+	return &resourcev1.RenewalNodeIdResp{}, nil
 }
 
 // ReleaseNodeId 释放节点id
-func (s *nodeIDV1Service) ReleaseNodeId(context.Context, *resources.ReleaseNodeIdReq) (*resources.ReleaseNodeIdResp, error) {
-	return &resources.ReleaseNodeIdResp{}, nil
+func (s *nodeIDV1Service) ReleaseNodeId(ctx context.Context, req *resourcev1.ReleaseNodeIdReq) (*resourcev1.ReleaseNodeIdResp, error) {
+	return &resourcev1.ReleaseNodeIdResp{}, nil
 }
