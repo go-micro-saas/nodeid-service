83f2558dde58493fad154380f86586acad0a7abb
chenkaiguang <chenkaiguang@realibox.com>
2024-10-08 16:42:34 +0800

    feat: dockerfile

--- devops/makefile_cicd.mk
+++ devops/makefile_cicd.mk
@@ -4,8 +4,9 @@
 build:
 	#docker build -t nodeid-service:v1.0.0 -f ./devops/Dockerfile .
 	docker build \
+		--build-arg APP_DIR=app \
 		--build-arg SERVICE_NAME=nodeid-service \
-		--build-arg VERSION=v1.0.0 \
-		-t nodeid-service:v1.0.0 \
+		--build-arg VERSION=v1.0.1 \
+		-t nodeid-service:v1.0.1 \
 		-f ./devops/Dockerfile .
 
