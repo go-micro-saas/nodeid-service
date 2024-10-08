ba88f9f61c2fa715fcca31aff7172949103a7cbd
chenkaiguang <chenkaiguang@realibox.com>
2024-10-08 17:04:23 +0800

    feat: dockerfile

--- devops/makefile_cicd.mk
+++ devops/makefile_cicd.mk
@@ -3,7 +3,11 @@
 # build :-->: service image
 build:
 	#docker build -t nodeid-service:v1.0.0 -f ./devops/Dockerfile .
+	#docker pull golang:1.22.8
+	#docker pull debian:stable-20240926-slim
 	docker build \
+		--build-arg BUILD_FROM_IMAGE=golang:1.22.8 \
+		--build-arg RUN_SERVICE_IMAGE=debian:stable-20240926-slim \
 		--build-arg APP_DIR=app \
 		--build-arg SERVICE_NAME=nodeid-service \
 		--build-arg VERSION=latest \
