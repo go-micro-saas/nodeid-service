2385e75b28e9a86ffeadaa3e42e4af3a0429bf59
chenkaiguang <chenkaiguang@realibox.com>
2024-11-26 11:59:36 +0800

    feat: golang-base-image

--- devops/docker-build/base-image/build.sh
+++ devops/docker-build/base-image/build.sh
@@ -1,7 +1,7 @@
 # /bin/sh
 
 export FROM_IMAGE_NAME=golang:1.22.8
-export BASE_IMAGE_NAME=go-micro-saas/base-image:latest
+export BASE_IMAGE_NAME=go-micro-saas/golang-base-image:latest
 export IS_EXIST_BASE_IMAGE=0
 docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "^${BASE_IMAGE_NAME}$" && export IS_EXIST_BASE_IMAGE=1 || echo "CANNOT FOUND ${BASE_IMAGE_NAME}"
 if [ "${IS_EXIST_BASE_IMAGE}" -eq 1 ]; then
