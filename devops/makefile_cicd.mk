791e5d9988ec58bb1b0ed2c63a3cc62506f7df2d
chenkaiguang <kaiguangchan@gmail.com>
2024-11-27 09:48:45 +0800

    feat: build service image

--- devops/makefile_cicd.mk
+++ devops/makefile_cicd.mk
@@ -2,16 +2,14 @@
 .PHONY: build-base-image
 build-base-image:
 	@echo "build :-->: build base image"
-	/bin/sh ./devops/docker-build/base-image/build_base_image.sh
-	/bin/sh ./devops/docker-build/base-image/build_release_image.sh
+	/bin/sh ./devops/docker-build/service-image/build_base_image.sh
+	/bin/sh ./devops/docker-build/service-image/build_release_image.sh
 
 # build-image
 .PHONY: build-service-image
 build-service-image:
 	@echo "build :-->: build service image"
-	#$(MAKE) build-base-image
-	/bin/sh ./devops/docker-build/service-image/build_base_image.sh
-	/bin/sh ./devops/docker-build/service-image/build_release_image.sh
+	$(MAKE) build-base-image
 	/bin/sh ./devops/docker-build/service-image/build_service_image.sh
 
 # build-image
